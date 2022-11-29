e6b732f69851e491880aea0a6a1fbccd5ef79ea2
hyejin <58923740+hyejinjeong9999@users.noreply.github.com>
2022-11-30 08:46:58 +0900

    프로그래머스 12928 (#7)
    * 약수의 합 (https://school.programmers.co.kr/learn/courses/30/lessons/12928)

---
+++ src/test/java/Lv1/programmers/Q12928/HyejinTest.java
@@ -0,0 +1,14 @@
+package Lv1.programmers.Q12928;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyejinTest {
+
+    @Test
+    void solution() {
+        var testcase = new Q12928Test();
+        testcase.test(new Hyejin());
+    }
+}
