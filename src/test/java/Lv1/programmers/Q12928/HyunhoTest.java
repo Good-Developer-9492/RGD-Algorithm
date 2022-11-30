13bd781b4d5666b46bc396dd794bf0f5b0834f9e
hyunho058 <hyunho058@naver.com>
2022-11-30 20:59:40 +0900

    프로그래머스 약수의 합 (https://school.programmers.co.kr/learn/courses/30/lessons/12928)

---
+++ src/test/java/Lv1/programmers/Q12928/HyunhoTest.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q12928;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input = Q12928Test.input();
+        var output = Q12928Test.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
