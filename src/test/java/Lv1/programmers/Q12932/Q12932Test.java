ea4d21a252f571849775b9abd1db05dc02f91e0f
hyejin <hyejinj810@gmail.com>
2022-12-02 00:35:54 +0900

    Q12932

---
+++ src/test/java/Lv1/programmers/Q12932/Q12932Test.java
@@ -0,0 +1,18 @@
+package Lv1.programmers.Q12932;
+
+import java.util.List;
+
+public class Q12932Test {
+    public static List<Long> input() {
+        return List.of(
+            12345L
+        );
+    }
+
+    public static List<int[]> output() {
+        return List.of(
+            new int[]{5, 4, 3, 2, 1}
+        );
+    }
+}
+
