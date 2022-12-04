5da8a46ff027f1deddad0a55a9351f3fa9e5ae82
hyejin <58923740+hyejinjeong9999@users.noreply.github.com>
2022-12-05 08:47:20 +0900

    프로그래머스 Q12932 (#19)

---
+++ src/test/java/Lv1/programmers/Q12932/HyejinTest.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q12932;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class HyejinTest {
+    @Test
+    void solution(){
+        var input = Q12932Test.input();
+        var output = Q12932Test.output();
+
+        var algorithm = new Hyejin();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertArrayEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
