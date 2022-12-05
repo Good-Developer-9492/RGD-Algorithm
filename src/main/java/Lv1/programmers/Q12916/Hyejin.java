01f0ca14d97ea6ca8a66e8deb511faab328fa758
hyejin <hyejinj810@gmail.com>
2022-12-05 20:11:47 +0900

    Q12916

--- src/main/java/Lv1/programmers/Q12916/Hyejin.java
+++ src/main/java/Lv1/programmers/Q12916/Hyejin.java
@@ -2,6 +2,7 @@ package Lv1.programmers.Q12916;
 
 public class Hyejin {
     boolean solution(String s) {
+        //(1.21ms, 74.4MB)
         String lower = s.toLowerCase();
 
         int countS = (int) lower.chars().filter(c->c=='p').count();
