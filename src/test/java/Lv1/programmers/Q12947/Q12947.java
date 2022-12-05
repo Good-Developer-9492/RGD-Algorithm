7a692bace7ae02a0cad064e106cf4b19273810a8
TunaHG <asdf0185@naver.com>
2022-12-05 09:07:23 +0900

    12월 2주차 풀이 문제 테스트케이스 추가

---
+++ src/test/java/Lv1/programmers/Q12947/Q12947.java
@@ -0,0 +1,27 @@
+package Lv1.programmers.Q12947;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class Q12947 {
+    public static List<Integer> input() {
+        return asList(
+            10,
+            11,
+            12,
+            13,
+            18
+        );
+    }
+
+    public static List<Boolean> output() {
+        return asList(
+            true,
+            false,
+            true,
+            false,
+            true
+        );
+    }
+}
