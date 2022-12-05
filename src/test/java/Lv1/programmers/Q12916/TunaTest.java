59fdf0516ee82032a7846d43b5371486413fcd18
TunaHG <asdf0185@naver.com>
2022-12-05 09:17:34 +0900

    프로그래머스 문자열 내 p와 y의 개수 (https://school.programmers.co.kr/learn/courses/30/lessons/12916)

---
+++ src/test/java/Lv1/programmers/Q12916/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q12916;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q12916.input();
+        var output = Q12916.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
