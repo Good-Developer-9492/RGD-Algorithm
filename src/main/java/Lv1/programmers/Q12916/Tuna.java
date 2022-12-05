fa632cfcafa2d4ee89f49e21cfda284b1b7b84ba
Hagyun Kim <asdf0185@naver.com>
2022-12-06 00:41:28 +0900

    프로그래머스 12916 (#20)
    문자열 내 p와 y의 개수 (https://school.programmers.co.kr/learn/courses/30/lessons/12916)

---
+++ src/main/java/Lv1/programmers/Q12916/Tuna.java
@@ -0,0 +1,27 @@
+package Lv1.programmers.Q12916;
+
+public class Tuna {
+    // (0.20ms, 75MB)
+    boolean solution(String s) {
+        String[] splitedInput = s.split("");
+
+        int pCount = 0;
+        int yCount = 0;
+        for (String str : splitedInput) {
+            if (str.equals("p") || str.equals("P"))
+                pCount++;
+            if (str.equals("y") || str.equals("Y"))
+                yCount++;
+        }
+
+        return pCount == yCount;
+    }
+
+    // 참고용, 다른사람의 풀이
+    // (1.5ms, 75MB)
+//    boolean solution(String s) {
+//        s = s.toUpperCase();
+//
+//        return s.chars().filter( e -> 'P'== e).count() == s.chars().filter( e -> 'Y'== e).count();
+//    }
+}
