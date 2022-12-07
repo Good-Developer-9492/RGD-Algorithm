f7a90f9cc65e68a595e9a36ae8751fcd4e686ba1
Hyunho Kim <hyunho058@naver.com>
2022-12-07 20:00:31 +0900

    프로그래머스 12925 (#25)

---
+++ src/test/java/Lv1/programmers/Q12925/HyunhoTest.java
@@ -0,0 +1,21 @@
+package Lv1.programmers.Q12925;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyunhoTest {
+
+    @Test
+    void solution() {
+        var input = Q12925.input();
+        var output = Q12925.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+
+}
