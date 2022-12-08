303102feea21ef34320d1935cba462c63dbdf71f
hyunho058 <hyunho058@naver.com>
2022-12-08 19:57:21 +0900

    프로그래머스 하샤드 수 (https://school.programmers.co.kr/learn/courses/30/lessons/12947)

---
+++ src/main/java/Lv1/programmers/Q12947/Hyunho.java
@@ -0,0 +1,15 @@
+package Lv1.programmers.Q12947;
+
+public class Hyunho {
+    //0.04ms, 82.8MB
+    public boolean solution(int x) {
+        int sum = 0;
+        int temp = x;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return x % sum == 0;
+    }
+}
