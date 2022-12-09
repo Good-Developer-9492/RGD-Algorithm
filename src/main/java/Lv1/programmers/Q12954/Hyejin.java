37be3922a9a0d7c64cc93d145339f8105b432823
hyejin <58923740+hyejinjeong9999@users.noreply.github.com>
2022-12-09 21:41:02 +0900

    프로그래머스 12947 (#28)

--- src/main/java/Lv1/programmers/Q12954/Hyejin.java
+++ src/main/java/Lv1/programmers/Q12954/Hyejin.java
@@ -1,8 +1,8 @@
 package Lv1.programmers.Q12954;
 
 public class Hyejin {
-
     public long[] solution(int x, int n) {
+        //(0.04ms, 72.4MB)
         long[] answer = new long[n];
         answer[0] = x;
         for (int i = 1; i < n; i++) {
