ab5682f5ecaf60d0c7d53cc44e14dd141c8f6dc2
Hyunho Kim <hyunho058@naver.com>
2022-12-09 22:20:34 +0900

    프로그래머스 12954 (#27)
    x만큼 간격이 있는 n개의 숫자 (https://school.programmers.co.kr/learn/courses/30/lessons/12954)

---
+++ src/main/java/Lv1/programmers/Q12954/Hyunho.java
@@ -0,0 +1,17 @@
+package Lv1.programmers.Q12954;
+
+public class Hyunho {
+    public long[] solution(int x, int n) {
+        //0.04ms, 79MB
+        long[] answer = new long[n];
+        long temp = x;
+
+        answer[0] = temp;
+        for (int i = 1; i < n; i++) {
+            temp += x;
+            answer[i] = temp;
+        }
+
+        return answer;
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q12954/HyunhoTest.java
@@ -0,0 +1,23 @@
+package Lv1.programmers.Q12954;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input1 = Q12954.input_1();
+        var input2 = Q12954.input_2();
+        var output = Q12954.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(
+                algorithm.solution(input1.get(i), input2.get(i)),
+                output.get(i));
+        }
+    }
+}
