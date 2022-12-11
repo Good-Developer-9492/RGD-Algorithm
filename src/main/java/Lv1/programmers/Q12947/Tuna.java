0b88ff0c375977a7f108582c69a79bcf3e344c11
Hagyun Kim <asdf0185@naver.com>
2022-12-12 08:28:48 +0900

    프로그래머스 12947 (#30)
    하샤드 수 (https://school.programmers.co.kr/learn/courses/30/lessons/12947)

---
+++ src/main/java/Lv1/programmers/Q12947/Tuna.java
@@ -0,0 +1,15 @@
+package Lv1.programmers.Q12947;
+
+public class Tuna {
+    // (0.02ms, 75MB)
+    public boolean solution(int x) {
+        int input = x;
+        int sum = 0;
+        while(x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+
+        return input % sum == 0;
+    }
+}
