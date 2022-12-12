b24f5ea853d478ac9783e9e50eb693f9d0cb3b5f
hyejin <hyejinj810@gmail.com>
2022-12-12 21:33:47 +0900

    프로그래머스 12912

--- src/main/java/Lv1/programmers/Q12912/Hyejin.java
+++ src/main/java/Lv1/programmers/Q12912/Hyejin.java
@@ -4,11 +4,11 @@ public class Hyejin {
     public long solution(int a, int b) {
         //7.68ms, 84.8MB
         int start = Math.min(a, b);
-        int end = Math.max(a,b);
+        int end = Math.max(a, b);
 
         long answer = 0;
-        for(int i = start; i<=end; i++){
-            answer+=i;
+        for (int i = start; i <= end; i++) {
+            answer += i;
         }
         return answer;
     }
