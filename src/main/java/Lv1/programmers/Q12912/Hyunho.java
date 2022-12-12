f7f17fe5dd1fef3e61a0149709a633cae6cc2ff2
hyunho058 <hyunho058@naver.com>
2022-12-12 21:02:54 +0900

    프로그래머스 두 정수 사이의 합 (https://school.programmers.co.kr/learn/courses/30/lessons/12912)

---
+++ src/main/java/Lv1/programmers/Q12912/Hyunho.java
@@ -0,0 +1,16 @@
+package Lv1.programmers.Q12912;
+
+public class Hyunho {
+    //14.50ms, 83.3MB
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+
+        for (int i = min; i <= max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
