5aa5c0573a69007b20e9a7e25872e5c238242261
hyunho058 <hyunho058@naver.com>
2022-12-13 19:10:07 +0900

    프로그래머스 콜라츠 추측 (https://school.programmers.co.kr/learn/courses/30/lessons/12943)

---
+++ src/main/java/Lv1/programmers/Q12843/Hyunho.java
@@ -0,0 +1,22 @@
+package Lv1.programmers.Q12843;
+
+public class Hyunho {
+    //0.04ms, 69.7MB
+    public int solution(int num) {
+        int answer = 0;
+
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else if (num % 2 == 1){
+                num *= 3;
+                num += 1;
+            }
+            answer++;
+            if (answer > 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
