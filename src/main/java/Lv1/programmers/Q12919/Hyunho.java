e77eaa03fb882a97d13d140e2cf20343e48779c5
hyunho058 <hyunho058@naver.com>
2022-12-14 19:54:34 +0900

    프로그래머스 서울에서 김서방 찾기 (https://school.programmers.co.kr/learn/courses/30/lessons/12919)

---
+++ src/main/java/Lv1/programmers/Q12919/Hyunho.java
@@ -0,0 +1,18 @@
+package Lv1.programmers.Q12919;
+
+public class Hyunho {
+    //0.13ms, 72.2MB
+    public String solution(String[] seoul) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("김서방은 ");
+
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")){
+                sb.append(i);
+                sb.append("에 있다");
+            }
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q12919/HyunhoTest.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q12919;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input = Q12919.input();
+        var output = Q12919.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+}
