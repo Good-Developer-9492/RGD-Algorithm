e67a84faba0d6a4f8ab3ab0c0546a1fddbc06ddc
TunaHG <asdf0185@naver.com>
2022-12-14 15:24:24 +0900

    프로그래머스 서울에서 김서방 찾기 (https://school.programmers.co.kr/learn/courses/30/lessons/12919)

---
+++ src/main/java/Lv1/programmers/Q12919/Tuna.java
@@ -0,0 +1,15 @@
+package Lv1.programmers.Q12919;
+
+public class Tuna {
+    // (1ms, 75MB)
+    public String solution(String[] seoul) {
+        String answer = "";
+        for (int i = 0; i < seoul.length; i++) {
+            if (seoul[i].equals("Kim")) {
+                answer = String.format("김서방은 %d에 있다", i);
+                break;
+            }
+        }
+        return answer;
+    }
+}
