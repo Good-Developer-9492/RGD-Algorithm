46e00db2fda655ec63ada5929bdca920c6f5be97
Hyunho Kim <hyunho058@naver.com>
2022-12-15 20:15:05 +0900

    프로그래머스 12943 (#42)
    * 프로그래머스 콜라츠 추측 (https://school.programmers.co.kr/learn/courses/30/lessons/12943)
    
    * 프로그래머스 콜라츠 추측 (https://school.programmers.co.kr/learn/courses/30/lessons/12943)
    
    * 타입 변경

---
+++ src/main/java/Lv1/programmers/Q12943/Hyunho.java
@@ -0,0 +1,23 @@
+package Lv1.programmers.Q12943;
+
+public class Hyunho {
+    //0.04ms, 69.7MB
+    public int solution(int num) {
+        int answer = 0;
+        long tempNum = (long) num;
+
+        while (tempNum != 1) {
+            if (tempNum % 2 == 0) {
+                tempNum = tempNum / 2;
+            } else if (tempNum % 2 == 1){
+                tempNum *= 3;
+                tempNum += 1;
+            }
+            answer++;
+            if (answer > 500) {
+                return -1;
+            }
+        }
+        return answer;
+    }
+}
