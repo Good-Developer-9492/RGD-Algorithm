3f22e25a7faae000b6c238677dca2b9ec682478f
summerr0-0 <summerr060@gmail.com>
2022-12-17 20:36:03 +0900

    아맞다시간

--- src/main/java/Lv1/programmers/Q12935/Hyejin.java
+++ src/main/java/Lv1/programmers/Q12935/Hyejin.java
@@ -3,6 +3,7 @@ package Lv1.programmers.Q12935;
 import java.util.Arrays;
 
 public class Hyejin {
+    //0.05ms, 76.7MB
     public int[] solution(int[] arr) {
         int min = Integer.MAX_VALUE;
         if (arr.length == 1) return new int[]{-1};
