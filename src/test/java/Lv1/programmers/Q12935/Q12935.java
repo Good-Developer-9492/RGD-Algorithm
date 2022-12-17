0d43ec9b5d335f9083d22dbd3953a747d31ddd2d
summerr0-0 <summerr060@gmail.com>
2022-12-17 20:31:35 +0900

    프로그래머스 12935

--- src/test/java/Lv1/programmers/Q12935/Q12935.java
+++ src/test/java/Lv1/programmers/Q12935/Q12935.java
@@ -5,17 +5,17 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class Q12935 {
-    public static List<Integer[]> input1() {
+    public static List<int[]> input1() {
         return asList(
-            new Integer[]{4, 3, 2, 1},
-            new Integer[]{10}
+            new int[]{4, 3, 2, 1},
+            new int[]{10}
         );
     }
 
-    public static List<Integer[]> output() {
+    public static List<int[]> output() {
         return asList(
-            new Integer[]{4, 3, 2},
-            new Integer[]{-1}
+            new int[]{4, 3, 2},
+            new int[]{-1}
         );
     }
 }
