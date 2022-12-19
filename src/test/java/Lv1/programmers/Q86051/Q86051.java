68f0786518612b06de8d43e80d59fd6a533d995e
TunaHG <asdf0185@naver.com>
2022-12-20 08:59:43 +0900

    프로그래머스 없는 숫자 더하기 (https://school.programmers.co.kr/learn/courses/30/lessons/86051)

--- src/test/java/Lv1/programmers/Q86051/Q86051.java
+++ src/test/java/Lv1/programmers/Q86051/Q86051.java
@@ -7,7 +7,7 @@ import static java.util.Arrays.asList;
 public class Q86051 {
     public static List<int[]> input() {
         return asList(
-            new int[]{1, 2, 3, 4, 5, 6, 7, 8, 0},
+            new int[]{1, 2, 3, 4, 6, 7, 8, 0},
             new int[]{5, 8, 4, 0, 6, 7, 9}
         );
     }
