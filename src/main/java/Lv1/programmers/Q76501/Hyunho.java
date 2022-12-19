9562222050effa4c04b22d621f9b3884a4f1959b
hyunho058 <hyunho058@naver.com>
2022-12-19 21:33:01 +0900

    프로그래머스 음양 더하기 (https://school.programmers.co.kr/learn/courses/30/lessons/76501)

---
+++ src/main/java/Lv1/programmers/Q76501/Hyunho.java
@@ -0,0 +1,17 @@
+package Lv1.programmers.Q76501;
+
+public class Hyunho {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]){
+                answer += absolutes[i];
+                continue;
+            }
+            answer -= absolutes[i];
+        }
+
+        return answer;
+    }
+}
