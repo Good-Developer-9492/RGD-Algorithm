8b7af20c78a91b283257d337a80cdebe091541e2
summerr0-0 <summerr060@gmail.com>
2022-12-20 15:15:14 +0900

    프로그래머스 86051

---
+++ src/main/java/Lv1/programmers/Q86051/Hyejin.java
@@ -0,0 +1,18 @@
+package Lv1.programmers.Q86051;
+
+
+public class Hyejin {
+    //0.03ms, 78.1MB
+    public int solution(int[] numbers) {
+        int answer = 0;
+
+        for (int i = 0; i < 10; i++)
+            answer += i;
+
+        for (int number : numbers)
+            answer -= number;
+
+        return answer;
+    }
+
+}
