b97aa1f0b89b67cd92a914cb709e520ea3e8b1b4
summerr0-0 <summerr060@gmail.com>
2022-12-22 10:06:15 +0900

    프로그래머스 12903

---
+++ src/test/java/Lv1/programmers/Q12903/HyunhoTest.java
@@ -0,0 +1,21 @@
+package Lv1.programmers.Q12903;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyunhoTest {
+
+    @Test
+    void solution() {
+        var input = Q12903.input();
+        var output = Q12903.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+
+    }
+}
