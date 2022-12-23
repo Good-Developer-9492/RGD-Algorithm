b296354de7e6336743f88ded90211865784bc028
Hyunho Kim <hyunho058@naver.com>
2022-12-23 20:06:08 +0900

    프로그래머스 12903 (#65)

---
+++ src/main/java/Lv1/programmers/Q12903/Hyunho.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q12903;
+
+public class Hyunho {
+    public String solution(String s) {
+        int wordLength = s.length();
+
+        if (isEven(s)) {
+            return String.valueOf(s.charAt((wordLength / 2) - 1)) + s.charAt((wordLength / 2));
+        }
+        return String.valueOf(s.charAt((wordLength / 2)));
+    }
+
+    private boolean isEven(String s) {
+        if (s.length() % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+}
