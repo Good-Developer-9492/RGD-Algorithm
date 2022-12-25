15944fc8f85dcdfcacc0e449596e88bc04a6f1a3
Hagyun Kim <asdf0185@naver.com>
2022-12-25 22:14:30 +0900

    프로그래머스 86051 (#59)
    없는 숫자 더하기 (https://school.programmers.co.kr/learn/courses/30/lessons/86051)

---
+++ src/test/java/Lv1/programmers/Q86051/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q86051;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q86051.input();
+        var output = Q86051.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
