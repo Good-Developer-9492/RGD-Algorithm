6d371a689d8db240ee62d7047b2550a00cc8de51
TunaHG <asdf0185@naver.com>
2022-12-26 15:30:26 +0900

    프로그래머스 약수의 개수와 덧셈 (https://school.programmers.co.kr/learn/courses/30/lessons/77884)

---
+++ src/main/java/Lv1/programmers/Q77884/Tuna.java
@@ -0,0 +1,32 @@
+package Lv1.programmers.Q77884;
+
+public class Tuna {
+    // (2ms, 70MB)
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for(int i = left; i <= right; i++) {
+            if(getDivisorCount(i) % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
+        }
+
+        return answer;
+    }
+
+    public int getDivisorCount(int num) {
+        int result = 0;
+
+        int idx = 1;
+        while(num >= idx) {
+            if (num % idx == 0) {
+                result++;
+            }
+            idx++;
+        }
+
+        return result;
+    }
+}
