5cfc7034cec461e6325d9f37de15646066c75522
hyunho058 <hyunho058@naver.com>
2022-12-28 18:30:23 +0900

    프로그래머스 부족한 금액 계산하기 (https://school.programmers.co.kr/learn/courses/30/lessons/82612)

---
+++ src/test/java/Lv1/programmers/Q82612/HyunhoTest.java
@@ -0,0 +1,22 @@
+package Lv1.programmers.Q82612;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input1 = Q82612.input1();
+        var input2 = Q82612.input2();
+        var input3 = Q82612.input3();
+
+        var output = Q82612.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            Assertions.assertEquals(output.get(i), algorithm.solution(input1.get(i), input2.get(i), input3.get(i)));
+        }
+    }
+
+}
