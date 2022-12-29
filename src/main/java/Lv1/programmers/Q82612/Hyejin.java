63c14f0e128dace598b11bc49fb4a97ffeef67d5
summerr0-0 <120109736+summerr0-0@users.noreply.github.com>
2022-12-29 19:47:30 +0900

    프로그래머스 82612 (#82)

---
+++ src/main/java/Lv1/programmers/Q82612/Hyejin.java
@@ -0,0 +1,13 @@
+package Lv1.programmers.Q82612;
+
+public class Hyejin {
+    public long solution(int price, int money, int count) {
+        //0.17ms, 71.2MB
+        long amount = 0;
+
+        while (!(count < 0))
+            amount += price * count--;
+
+        return Math.max(amount - money, 0);
+    }
+}
