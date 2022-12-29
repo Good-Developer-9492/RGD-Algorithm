6d17e6000bb386ce22ffece3529d43f7c7fd81b4
hyunho058 <hyunho058@naver.com>
2022-12-29 19:57:46 +0900

    프로그래머스 행렬의 덧셈 (https://school.programmers.co.kr/learn/courses/30/lessons/12950)

---
+++ src/main/java/Lv1/programmers/Q12950/Hyunho.java
@@ -0,0 +1,16 @@
+package Lv1.programmers.Q12950;
+
+public class Hyunho {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        //6.66ms, 125MB
+        int[][] answer = new int[arr1.length][arr1[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
