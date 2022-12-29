080b0dc8ef4c5f3a01c6e75219faa1fde8c0e448
Hyunho Kim <hyunho058@naver.com>
2022-12-29 19:32:51 +0900

    프로그래머스 82612 (#81)

---
+++ src/main/java/Lv1/programmers/Q82612/Hyunho.java
@@ -0,0 +1,18 @@
+package Lv1.programmers.Q82612;
+
+public class Hyunho {
+    //0.21ms, 72.9MB
+    public long solution(int price, int money, int count) {
+        long totalMoney = 0;
+
+        for (int i = 1; i <= count; i++) {
+            totalMoney += (long) price * i;
+        }
+
+        if (money < totalMoney) {
+            return totalMoney - money;
+        }
+
+        return 0;
+    }
+}
