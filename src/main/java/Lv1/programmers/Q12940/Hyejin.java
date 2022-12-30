ea427985ee6c7930e3d888779f4151ff6692f440
summerr0-0 <summerr060@gmail.com>
2022-12-30 10:46:35 +0900

    프로그래머스 최대공약수와 최소공배수 (https://school.programmers.co.kr/learn/courses/30/lessons/12940)

---
+++ src/main/java/Lv1/programmers/Q12940/Hyejin.java
@@ -0,0 +1,15 @@
+package Lv1.programmers.Q12940;
+
+public class Hyejin {
+    public int[] solution(int n, int m) {
+        int a = Math.max(n, m);
+        int b = Math.min(n, m);
+
+        int gcd = gcd(a, b);
+        return new int[]{gcd, (n * m) / gcd};
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
