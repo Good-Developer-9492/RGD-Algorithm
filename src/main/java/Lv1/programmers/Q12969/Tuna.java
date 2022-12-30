d1d79493e5cce0f1a781a4eca2b8b59299479577
TunaHG <asdf0185@naver.com>
2022-12-30 23:46:36 +0900

    프로그래머스 직사각형 별찍기 (https://school.programmers.co.kr/learn/courses/30/lessons/12969)

---
+++ src/main/java/Lv1/programmers/Q12969/Tuna.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q12969;
+
+import java.util.Scanner;
+
+public class Tuna {
+    // (150ms, 70MB)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
