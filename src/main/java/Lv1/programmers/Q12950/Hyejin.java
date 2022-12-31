0da020064cd8a8a667e495040b31b66be71a9f9c
summerr0-0 <120109736+summerr0-0@users.noreply.github.com>
2022-12-31 19:24:11 +0900

    프로그래머스 12950 (#84)

---
+++ src/main/java/Lv1/programmers/Q12950/Hyejin.java
@@ -0,0 +1,14 @@
+package Lv1.programmers.Q12950;
+
+public class Hyejin {
+    //0.08ms, 80.1MB
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
