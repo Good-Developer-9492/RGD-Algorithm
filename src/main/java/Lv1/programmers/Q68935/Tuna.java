80ab42f84d48b1a30a0d0a89b4a8684ace9dda07
Hagyun Kim <asdf0185@naver.com>
2023-01-04 08:50:31 +0900

    프로그래머스 68935 (#96)
    3진법 뒤집기 (https://school.programmers.co.kr/learn/courses/30/lessons/68935)

---
+++ src/main/java/Lv1/programmers/Q68935/Tuna.java
@@ -0,0 +1,21 @@
+package Lv1.programmers.Q68935;
+
+public class Tuna {
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n >= 3) {
+            sb.append(n % 3);
+            n /= 3;
+        }
+        sb.append(n);
+
+        int answer = sb.charAt(sb.length() - 1) - '0';
+        int pow = 3;
+        for (int i = sb.length() - 2; i >= 0; i--) {
+            answer += (sb.charAt(i) - '0') * pow;
+            pow *= 3;
+        }
+
+        return answer;
+    }
+}
