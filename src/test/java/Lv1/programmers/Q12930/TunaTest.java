511e331d093694cf0cc14cd0cad70d83cf174070
Hagyun Kim <asdf0185@naver.com>
2023-01-04 08:50:07 +0900

    프로그래머스 12930 (#93)
    이상한 문자 만들기 (https://school.programmers.co.kr/learn/courses/30/lessons/12930)

---
+++ src/test/java/Lv1/programmers/Q12930/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q12930;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q12930.input();
+        var output = Q12930.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
