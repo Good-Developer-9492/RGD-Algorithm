926d3395b9a26cd75777688ee3b65f85bbc1d72c
Hagyun Kim <asdf0185@naver.com>
2023-01-07 10:14:32 +0900

    프로그래머스 86491 (#103)
    최소직사각형 (https://school.programmers.co.kr/learn/courses/30/lessons/86491)

---
+++ src/main/java/Lv1/programmers/Q86491/Tuna.java
@@ -0,0 +1,21 @@
+package Lv1.programmers.Q86491;
+
+public class Tuna {
+    // (0.3ms, 80MB)
+    public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxLength = 0;
+
+        for (int[] size : sizes) {
+            if (size[0] >= size[1]) {
+                maxWidth = Math.max(maxWidth, size[0]);
+                maxLength = Math.max(maxLength, size[1]);
+            } else {
+                maxWidth = Math.max(maxWidth, size[1]);
+                maxLength = Math.max(maxLength, size[0]);
+            }
+        }
+
+        return maxWidth * maxLength;
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q86491/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q86491;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q86491.input();
+        var output = Q86491.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
