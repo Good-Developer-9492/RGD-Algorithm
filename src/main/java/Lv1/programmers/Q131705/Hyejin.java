03f8796055feb9c341b08d4d2bab1dd5700a17d6
summerr0-0 <summerr060@gmail.com>
2023-01-09 21:43:21 +0900

    프로그래머스 삼총사 (https://school.programmers.co.kr/learn/courses/30/lessons/131705)

---
+++ src/main/java/Lv1/programmers/Q131705/Hyejin.java
@@ -0,0 +1,16 @@
+package Lv1.programmers.Q131705;
+
+public class Hyejin {
+    public int solution(int[] number) {
+        int result = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i+1; j < number.length; j++) {
+                for (int k = j+1; k < number.length; k++) {
+                    if (i == j || i == k || j == k) continue;
+                    if (number[i] + number[j] + number[k] == 0) result++;
+                }
+            }
+        }
+        return result;
+    }
+}
