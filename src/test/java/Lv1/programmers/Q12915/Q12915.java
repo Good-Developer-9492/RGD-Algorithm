de8918b6623f5a99423eb933546db2c8d38b99c6
TunaHG <asdf0185@naver.com>
2023-01-10 08:57:46 +0900

    프로그래머스 문자열 내 마음대로 정렬하기 (https://school.programmers.co.kr/learn/courses/30/lessons/12915)

--- src/test/java/Lv1/programmers/Q12915/Q12915.java
+++ src/test/java/Lv1/programmers/Q12915/Q12915.java
@@ -5,10 +5,17 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class Q12915 {
-    public static List<String[]> input() {
+    public static List<String[]> input1() {
         return asList(
             new String[]{"sun", "bed", "car"},
             new String[]{"abce", "abcd", "cdx"}
+        );
+    }
+
+    public static List<Integer> input2() {
+        return asList(
+            1,
+            2
         );
     }
 
