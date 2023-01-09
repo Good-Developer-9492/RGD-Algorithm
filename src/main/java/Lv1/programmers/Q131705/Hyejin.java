fbdb9428437077f0adfd51f957041c5f04a70046
summerr0-0 <summerr060@gmail.com>
2023-01-09 21:56:58 +0900

    시간 추가

--- src/main/java/Lv1/programmers/Q131705/Hyejin.java
+++ src/main/java/Lv1/programmers/Q131705/Hyejin.java
@@ -1,6 +1,7 @@
 package Lv1.programmers.Q131705;
 
 public class Hyejin {
+//    0.03ms, 71.7MB
     public int solution(int[] number) {
         int result = 0;
         for (int i = 0; i < number.length; i++) {
