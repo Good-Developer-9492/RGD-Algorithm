20e78baad3d47978536d2904b572959286ff7bf1
Hagyun Kim <asdf0185@naver.com>
2023-01-10 08:40:37 +0900

    프로그래머스 17681 (#106)
    [1차] 비밀지도 (https://school.programmers.co.kr/learn/courses/30/lessons/17681)

---
+++ src/test/java/Lv1/programmers/Q17681/TunaTest.java
@@ -0,0 +1,22 @@
+package Lv1.programmers.Q17681;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input1 = Q17681.input1();
+        var input2 = Q17681.input2();
+        var input3 = Q17681.input3();
+        var output = Q17681.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(algorithm.solution(input1.get(i), input2.get(i), input3.get(i)), output.get(i));
+        }
+    }
+}
