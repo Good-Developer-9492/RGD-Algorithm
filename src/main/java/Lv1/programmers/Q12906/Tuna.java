71b5d28baa4cf3a9ca8f7ebbb24d9bbf7d0646a6
Hagyun Kim <asdf0185@naver.com>
2023-01-10 08:41:08 +0900

    프로그래머스 12906 (#92)
    같은 숫자는 싫어 (https://school.programmers.co.kr/learn/courses/30/lessons/12906)

---
+++ src/main/java/Lv1/programmers/Q12906/Tuna.java
@@ -0,0 +1,22 @@
+package Lv1.programmers.Q12906;
+
+import java.util.ArrayList;
+
+public class Tuna {
+    // 정확성 (0.05ms, 74MB), 효율성 (25ms, 115MB)
+    public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) continue;
+            list.add(arr[i]);
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
