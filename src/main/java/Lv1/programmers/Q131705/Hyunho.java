9f1f37bccaa44a4a784ff6c61cf445cef2737478
hyunho058 <hyunho058@naver.com>
2023-01-09 21:02:14 +0900

    프로그래머스 삼총사 (https://school.programmers.co.kr/learn/courses/30/lessons/131705)

---
+++ src/main/java/Lv1/programmers/Q131705/Hyunho.java
@@ -0,0 +1,16 @@
+package Lv1.programmers.Q131705;
+
+public class Hyunho {
+    //0.03ms, 71.6MB
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
