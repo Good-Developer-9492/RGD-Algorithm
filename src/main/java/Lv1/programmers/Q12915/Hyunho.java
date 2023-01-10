a3af3f085e1b1b021229d4df1d1713fd067d760f
hyunho058 <hyunho058@naver.com>
2023-01-10 20:50:10 +0900

    프로그래머스 문자열 내 마음대로 정렬하기 (https://school.programmers.co.kr/learn/courses/30/lessons/12915)

---
+++ src/main/java/Lv1/programmers/Q12915/Hyunho.java
@@ -0,0 +1,17 @@
+package Lv1.programmers.Q12915;
+
+import java.util.Arrays;
+
+public class Hyunho {
+    public String[] solution(String[] strings, int n) {
+        return Arrays.stream(strings).sorted((o1, o2) -> {
+            if (o1.charAt(n) < o2.charAt(n)) {
+                return -1;
+            } else if (o1.charAt(n) == o2.charAt(n)) {
+                return o1.compareTo(o2);
+            } else {
+                return 1;
+            }
+        }).toArray(String[]::new);
+    }
+}
