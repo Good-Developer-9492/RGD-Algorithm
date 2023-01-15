17a67b968adaefc014c72a61bf86846248c066d1
TunaHG <asdf0185@naver.com>
2023-01-16 08:01:36 +0900

    23년 1월 3주차 테스트케이스 추가

---
+++ src/test/java/Lv1/programmers/Q132267/Q132267.java
@@ -0,0 +1,35 @@
+package Lv1.programmers.Q132267;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class Q132267 {
+    public static List<Integer> input1() {
+        return asList(
+            2,
+            3
+        );
+    }
+
+    public static List<Integer> input2() {
+        return asList(
+            1,
+            1
+        );
+    }
+
+    public static List<Integer> input3() {
+        return asList(
+            20,
+            20
+        );
+    }
+
+    public static List<Integer> output() {
+        return asList(
+            19,
+            9
+        );
+    }
+}
