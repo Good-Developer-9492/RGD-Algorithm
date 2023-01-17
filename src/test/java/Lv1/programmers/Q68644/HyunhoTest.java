25ad432a812130dcaf5bc38a1eb041437cf48516
hyunho058 <hyunho058@naver.com>
2023-01-17 20:05:55 +0900

    프로그래머스 두 개 뽑아서 더하기 (https://school.programmers.co.kr/learn/courses/30/lessons/68644)

---
+++ src/test/java/Lv1/programmers/Q68644/HyunhoTest.java
@@ -0,0 +1,18 @@
+package Lv1.programmers.Q68644;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input = Q68644.input();
+        var output = Q68644.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            Assertions.assertArrayEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+}
