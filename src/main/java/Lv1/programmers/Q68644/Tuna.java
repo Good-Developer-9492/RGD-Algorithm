811309ecd2e91e5b51f44528302e72e90ddb5e67
Hagyun Kim <asdf0185@naver.com>
2023-01-19 08:13:51 +0900

    프로그래머스 68644 (#121)
    두 개 뽑아서 더하기 (https://school.programmers.co.kr/learn/courses/30/lessons/68644)

---
+++ src/main/java/Lv1/programmers/Q68644/Tuna.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q68644;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Tuna {
+    // (4ms, 77MB)
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        return set.stream().sorted().mapToInt(Integer::intValue).toArray();
+    }
+}
