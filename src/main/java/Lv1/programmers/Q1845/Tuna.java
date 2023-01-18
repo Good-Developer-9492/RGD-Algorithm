4ed7ca91d2ef38feaead761f1d221cdff5b5be88
TunaHG <asdf0185@naver.com>
2023-01-19 08:41:23 +0900

    프로그래머스 폰켓몬 (https://school.programmers.co.kr/learn/courses/30/lessons/1845)

---
+++ src/main/java/Lv1/programmers/Q1845/Tuna.java
@@ -0,0 +1,17 @@
+package Lv1.programmers.Q1845;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Tuna {
+    // (0.2ms, 75MB)
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        return Math.min(set.size(), nums.length / 2);
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q1845/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q1845;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q1845.input();
+        var output = Q1845.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
