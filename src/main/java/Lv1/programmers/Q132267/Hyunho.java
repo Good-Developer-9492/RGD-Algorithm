2a5590fa06a361877fa8aee6948cee41a1945c77
hyunho058 <hyunho058@naver.com>
2023-01-20 20:29:58 +0900

    프로그래머스 콜라 문제 (https://school.programmers.co.kr/learn/courses/30/lessons/132267)

---
+++ src/main/java/Lv1/programmers/Q132267/Hyunho.java
@@ -0,0 +1,15 @@
+package Lv1.programmers.Q132267;
+
+public class Hyunho {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+
+        while (n >= a){
+            int coke = (n / a) * b;
+            int remainder = n % a;
+            answer += coke;
+            n = coke + remainder;
+        }
+        return answer;
+    }
+}
