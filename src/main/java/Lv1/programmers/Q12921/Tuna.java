ec6c61a6c3447a5d95451eb573aab02234e48f75
TunaHG <asdf0185@naver.com>
2023-01-21 17:43:49 +0900

    프로그래머스 소수 찾기 (https://school.programmers.co.kr/learn/courses/30/lessons/12921)

---
+++ src/main/java/Lv1/programmers/Q12921/Tuna.java
@@ -0,0 +1,26 @@
+package Lv1.programmers.Q12921;
+
+public class Tuna {
+    // (0.5 ~ 8.8ms / 68 ~ 85MB)
+    public int solution(int n) {
+        int maxSize = 1000001;
+        boolean[] arr = new boolean[maxSize];
+        arr[0] = true;
+        arr[1] = true;
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (arr[i]) continue;
+
+            count++;
+
+            int j = 2;
+            while (i * j <= n) {
+                arr[i * j] = true;
+                j++;
+            }
+        }
+
+        return count;
+    }
+}
