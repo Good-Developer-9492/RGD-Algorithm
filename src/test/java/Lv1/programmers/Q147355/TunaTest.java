928838a403416301bf8d4c0d2f68f98bf5cad2f5
TunaHG <asdf0185@naver.com>
2023-01-24 14:59:59 +0900

    프로그래머스 크기가 작은 부분 문자열 (https://school.programmers.co.kr/learn/courses/30/lessons/147355)

--- src/test/java/Lv1/programmers/Q147355/TunaTest.java
+++ src/test/java/Lv1/programmers/Q147355/TunaTest.java
@@ -8,5 +8,14 @@ class TunaTest {
 
     @Test
     void solution() {
+        var input1 = Q147355.input1();
+        var input2 = Q147355.input2();
+        var output = Q147355.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertEquals(algorithm.solution(input1.get(i), input2.get(i)), output.get(i));
+        }
     }
 }
