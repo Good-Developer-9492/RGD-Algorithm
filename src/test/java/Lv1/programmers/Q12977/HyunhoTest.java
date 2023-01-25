9830f47fd9cdd3d5d56c84f3e648c26e77406dca
hyunho058 <hyunho058@naver.com>
2023-01-25 19:19:53 +0900

    프로그래머스 소수 만들기 (https://school.programmers.co.kr/learn/courses/30/lessons/12977)

---
+++ src/test/java/Lv1/programmers/Q12977/HyunhoTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q12977;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input = Q12977.input();
+        var output = Q12977.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+
+}
