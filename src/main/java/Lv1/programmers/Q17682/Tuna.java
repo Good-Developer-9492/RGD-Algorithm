e66dfc064206594a9b4c4dfe112fb9c8cdf2e8e7
Hagyun Kim <asdf0185@naver.com>
2023-01-30 08:00:19 +0900

    프로그래머스 17682 (#147)
    [1차] 다트 게임 (https://school.programmers.co.kr/learn/courses/30/lessons/17682)

---
+++ src/main/java/Lv1/programmers/Q17682/Tuna.java
@@ -0,0 +1,59 @@
+package Lv1.programmers.Q17682;
+
+public class Tuna {
+    // (0.25ms, 75MB)
+    public int solution(String dartResult) {
+        String[] arr = dartResult.split("");
+
+        int answer = 0;
+        int[] score = new int[3];
+        int scoreIndex = 0;
+        int beforeArr = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (isNumber(arr[i])) {
+                if (beforeArr == 0) {
+                    beforeArr = Integer.parseInt(arr[i]);
+                } else {
+                    beforeArr = beforeArr * 10 + Integer.parseInt(arr[i]);
+                }
+            } else if (isBonus(arr[i])) {
+                if (arr[i].equals("D")) {
+                    beforeArr = (int) Math.pow(beforeArr, 2);
+                } else if (arr[i].equals("T")) {
+                    beforeArr = (int) Math.pow(beforeArr, 3);
+                }
+                score[scoreIndex++] = beforeArr;
+                answer += beforeArr;
+                beforeArr = 0;
+            } else if (isOption(arr[i])) {
+                if (arr[i].equals("*")) {
+                    if (scoreIndex == 1) {
+                        answer += score[scoreIndex - 1];
+                        score[scoreIndex - 1] *= 2;
+                    } else {
+                        answer += score[scoreIndex - 2] + score[scoreIndex - 1];
+                        score[scoreIndex - 2] *= 2;
+                        score[scoreIndex - 1] *= 2;
+                    }
+                } else if (arr[i].equals("#")) {
+                    score[scoreIndex - 1] = -score[scoreIndex - 1];
+                    answer += score[scoreIndex - 1] * 2;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    private boolean isNumber(String s) {
+        return s.matches("\\d");
+    }
+
+    private boolean isBonus(String s) {
+        return s.equals("S") || s.equals("D") || s.equals("T");
+    }
+
+    private boolean isOption(String s) {
+        return s.equals("*") || s.equals("#");
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q17682/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q17682;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q17682.input();
+        var output = Q17682.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
