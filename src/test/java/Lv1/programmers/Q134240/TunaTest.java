c2913331b18950ae8d91c8cdd657fbb3e6d4fa68
Hagyun Kim <asdf0185@naver.com>
2023-01-30 07:59:33 +0900

    프로그래머스 134240 (#143)
    푸드 파이트 대회 (https://school.programmers.co.kr/learn/courses/30/lessons/134240)

---
+++ src/test/java/Lv1/programmers/Q134240/TunaTest.java
@@ -0,0 +1,20 @@
+package Lv1.programmers.Q134240;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q134240.input();
+        var output = Q134240.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
