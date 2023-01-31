218e9bafc995b7c37c514bec51a17e6f8a8c1498
summerr0-0 <120109736+summerr0-0@users.noreply.github.com>
2023-01-31 19:19:20 +0900

    프로그래머스 134240 (#149)

---
+++ src/main/java/Lv1/programmers/Q134240/Hyejin.java
@@ -0,0 +1,25 @@
+package Lv1.programmers.Q134240;
+
+public class Hyejin {
+
+    public String solution(int[] food) {
+        //0.04ms, 68.5MB
+        //2.33ms, 85MB
+        StringBuilder sb = new StringBuilder();
+
+        int index = 0;
+
+        for (int i = 1; i < food.length; i++) {
+            if (food[i] == 1) continue;
+            int repeat = food[i] / 2;
+
+            for (int j = 0; j < repeat; j++) {
+                sb.insert(index, i);
+                sb.insert(sb.length() - index, i);
+                index++;
+            }
+        }
+        sb.insert(index, 0);
+        return sb.toString();
+    }
+}
