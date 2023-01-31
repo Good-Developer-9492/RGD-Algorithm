dd5ab863b287cef305a9b396cd8dd13ba656b57d
TunaHG <asdf0185@naver.com>
2023-02-01 08:56:25 +0900

    프로그래머스 과일 장수 (https://school.programmers.co.kr/learn/courses/30/lessons/135808)

---
+++ src/test/java/Lv1/programmers/Q135808/TunaTest.java
@@ -0,0 +1,22 @@
+package Lv1.programmers.Q135808;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input1 = Q135808.input1();
+        var input2 = Q135808.input2();
+        var input3 = Q135808.input3();
+        var output = Q135808.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertEquals(algorithm.solution(input1.get(i), input2.get(i), input3.get(i)), output.get(i));
+        }
+    }
+}
