fd220845a2febd5f8042435a42821c0fe996a628
summerr0-0 <summerr060@gamil.com>
2023-02-02 08:59:21 +0900

    42862 테스트케이스 추가

--- src/test/java/Lv1/programmers/Q42862/Q42862.java
+++ src/test/java/Lv1/programmers/Q42862/Q42862.java
@@ -2,7 +2,6 @@ package Lv1.programmers.Q42862;
 
 import java.util.List;
 
-//https://school.programmers.co.kr/learn/courses/30/lessons/42862
 import static java.util.Arrays.asList;
 
 public class Q42862 {
@@ -10,7 +9,11 @@ public class Q42862 {
         return asList(
             5,
             5,
-            3
+            3,
+            5,
+            7,
+            3,
+            13
         );
     }
 
@@ -18,7 +21,11 @@ public class Q42862 {
         return asList(
             new int[]{2, 4},
             new int[]{2, 4},
-            new int[]{3}
+            new int[]{3},
+            new int[]{2,4},
+            new int[]{2,4,7},
+            new int[]{1,2,3},
+            new int[]{1, 2, 5, 6, 10, 12, 13}
         );
     }
 
@@ -26,7 +33,12 @@ public class Q42862 {
         return asList(
             new int[]{1, 3, 5},
             new int[]{3},
-            new int[]{1}
+            new int[]{1},
+            new int[]{3,5},
+            new int[]{1,3,5},
+            new int[]{},
+            new int[]{2, 3, 4, 5, 7, 8, 9, 10, 11, 12}
+
         );
     }
 
@@ -34,7 +46,11 @@ public class Q42862 {
         return asList(
             5,
             4,
-            2
+            2,
+            5,
+            6,
+            0,
+            11
         );
     }
 }
