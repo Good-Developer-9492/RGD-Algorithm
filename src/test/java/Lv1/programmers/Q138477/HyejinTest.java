7b50368b82987feaccdb728d5082a27ef65ece1d
summerr0-0 <120109736+summerr0-0@users.noreply.github.com>
2023-02-05 16:49:01 +0900

    프로그래머스 138477 (#165)

---
+++ src/test/java/Lv1/programmers/Q138477/HyejinTest.java
@@ -0,0 +1,21 @@
+package Lv1.programmers.Q138477;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class HyejinTest {
+    @Test
+    void solution() {
+        var input1 = Q138477.input1();
+        var input2 = Q138477.input2();
+        var output = Q138477.output();
+
+        var algorithm = new Hyejin();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(output.get(i), algorithm.solution(input1.get(i), input2.get(i)));
+        }
+
+    }
+}
