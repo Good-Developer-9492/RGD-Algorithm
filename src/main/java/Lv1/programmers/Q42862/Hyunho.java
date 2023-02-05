f1b080abe85b5dca47144c7c76b06285e04c5a3d
hyunho058 <hyunho058@naver.com>
2023-02-05 16:38:09 +0900

    프로그래머스 완주하지 못한 선수 (https://school.programmers.co.kr/learn/courses/30/lessons/42576)

--- src/main/java/Lv1/programmers/Q42862/Hyunho.java
+++ src/main/java/Lv1/programmers/Q42862/Hyunho.java
@@ -1,41 +1,50 @@
 package Lv1.programmers.Q42862;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Hyunho {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        List<Integer> queue = new ArrayList<>();
+        int answer = 0;
+        Queue<Integer> reserveQueue = new LinkedList<>();
 
-        for (int i = 0; i < lost.length; i++) {
-            for (int j = 0; j < reserve.length; j++) {
-                if (lost[i] == reserve[j]){
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
-                if (i == lost.length -1){
-                    queue.add(reserve[j]);
-                }
-            }
-        }
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
 
         int tempIdx = 0;
-
-        for (int l : lost) {
-            if (l == -1) {
-                answer++;
-            }else {
-                for (int i = tempIdx; i < queue.size(); i++) {
-                    if (l - 1 == queue.get(i) || l + 1 == queue.get(i)) {
-                        answer++;
-                        tempIdx ++;
-                        break;
-                    }
+        for (int i = 0; i < lost.length; i++) {
+            for (int j = tempIdx; j < reserve.length; j++) {
+                if (lost[i] == reserve[j]) {
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    tempIdx = j;
+                    break;
                 }
             }
         }
+
+        for (int r : reserve){
+            if (r != -1)
+                reserveQueue.add(r);
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            if (lost[i] == -1) {
+                answer++;
+                continue;
+            }
+            for (int j = 0; j < reserveQueue.size(); j++) {
+                if (lost[i] -1 == reserveQueue.peek() || lost[i] +1 == reserveQueue.peek()){
+                    reserveQueue.remove();
+                    answer++;
+                    break;
+                }else if (lost[i] > reserveQueue.peek()){
+                    reserveQueue.remove();
+                }
+            }
+        }
+
+        answer += n - lost.length;
+
         return answer;
     }
 }
