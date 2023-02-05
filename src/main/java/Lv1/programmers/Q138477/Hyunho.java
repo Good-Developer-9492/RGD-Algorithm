aa41fe410a02f4c1216aa38667a8bbb29e291699
Hyunho Kim <hyunho058@naver.com>
2023-02-05 16:49:28 +0900

    프로그래머스 138477 (#166)

---
+++ src/main/java/Lv1/programmers/Q138477/Hyunho.java
@@ -0,0 +1,29 @@
+package Lv1.programmers.Q138477;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Hyunho {
+    //0.19ms, 75MB - 6.12ms, 86.1MB
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        List<Integer> ranking = new ArrayList<>();
+
+        for (int i = 0; i < score.length; i++) {
+            if (ranking.size() >= k && score[i] > ranking.get(0)) {
+                ranking.remove(0);
+            }
+
+            if (ranking.size() < k) {
+                ranking.add(score[i]);
+            }
+
+            Collections.sort(ranking);
+
+            answer[i] = ranking.get(0);
+        }
+
+        return answer;
+    }
+}
