11b022741e39ea2d3a69836260edd4cec60463eb
Hyunho Kim <hyunho058@naver.com>
2023-02-06 21:17:48 +0900

    프로그래머스 42576 (#168)

---
+++ src/main/java/Lv1/programmers/Q42576/Hyunho.java
@@ -0,0 +1,27 @@
+package Lv1.programmers.Q42576;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hyunho {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String, Integer> participantMap = new HashMap<>();
+        for (String s : participant) {
+            participantMap.put(s, participantMap.getOrDefault(s, 0)+1);
+        }
+
+        for (String s : completion) {
+            participantMap.put(s, participantMap.getOrDefault(s, 0)-1);
+        }
+
+        for (String s : participantMap.keySet()) {
+            if (participantMap.get(s) != 0){
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+}
