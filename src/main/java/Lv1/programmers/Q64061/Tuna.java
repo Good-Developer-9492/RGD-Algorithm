3a6f08d05a1fd00bfefea04e002437e6ae4c49f9
TunaHG <asdf0185@naver.com>
2023-02-09 08:28:56 +0900

    프로그래머스 크레인 인형뽑기 게임 (https://school.programmers.co.kr/learn/courses/30/lessons/64061)

---
+++ src/main/java/Lv1/programmers/Q64061/Tuna.java
@@ -0,0 +1,29 @@
+package Lv1.programmers.Q64061;
+
+import java.util.Stack;
+
+public class Tuna {
+    // (0.4ms / 76MB)
+    public int solution(int[][] board, int[] moves) {
+        Stack<Integer> stack = new Stack<>();
+        int answer = 0;
+
+        for (int move : moves) {
+            for (int[] line : board) {
+                if (line[move - 1] != 0) {
+                    if (!stack.isEmpty() && stack.peek() == line[move - 1]) {
+                        stack.pop();
+                        line[move - 1] = 0;
+                        answer += 2;
+                        break;
+                    }
+                    stack.push(line[move - 1]);
+                    line[move - 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
