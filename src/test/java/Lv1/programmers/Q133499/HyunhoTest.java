effee7082e4e9dc988074108034e048cbf51454b
hyunho058 <hyunho058@naver.com>
2023-02-10 19:22:41 +0900

    프로그래머스 옹알이(2) (https://school.programmers.co.kr/learn/courses/30/lessons/133499)

---
+++ src/test/java/Lv1/programmers/Q133499/HyunhoTest.java
@@ -0,0 +1,19 @@
+package Lv1.programmers.Q133499;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input = Q133499.input();
+        var output = Q133499.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+}
