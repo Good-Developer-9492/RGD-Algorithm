58e5f8bc236d868e7901de1cbf1a966e9d848d28
summerr0-0 <summerr060@gamil.com>
2023-02-10 10:19:04 +0900

    프로그래머스 133499

---
+++ src/main/java/Lv1/programmers/Q133499/Hyejin.java
@@ -0,0 +1,26 @@
+package Lv1.programmers.Q133499;
+
+public class Hyejin {
+
+    public int solution(String[] babbling) {
+        int count = 0;
+        String[] ableWords = {"aya", "woo", "ye", "ma"};
+        String[] repeatWords = {"ayaaya", "yeye", "woowoo", "mama"};
+
+        case1:
+        for (String word : babbling) {
+            for (String rw : repeatWords) {
+                if (word.contains(rw)) continue case1;
+            }
+
+            for (String aw : ableWords) {
+                word = word.replaceAll(aw, "1");
+            }
+            word = word.replaceAll("1","");
+            if (word.length() == 0) count++;
+
+        }
+        return count;
+    }
+
+}
