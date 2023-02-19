616402a83749d85509369afcc21ffe104b56e99a
summerr0-0 <summerr060@gamil.com>
2023-02-20 07:17:08 +0900

    프로그래머스 23년 2월 4주차 테스트케이스 추가

---
+++ src/test/java/Lv1/programmers/Q12909/Q12909.java
@@ -0,0 +1,25 @@
+package Lv1.programmers.Q12909;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class Q12909 {
+    public static List<String> input() {
+        return asList(
+            "()()",
+            "(())()",
+            ")()(",
+            "(()("
+        );
+    }
+
+    public static List<Boolean> output() {
+        return asList(
+            true,
+            true,
+            false,
+            false
+        );
+    }
+}
---
+++ src/test/java/Lv1/programmers/Q12941/Q12941.java
@@ -0,0 +1,28 @@
+package Lv1.programmers.Q12941;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class Q12941 {
+    public static List<int[]> input1() {
+        return asList(
+            new int[]{1, 4, 2},
+            new int[]{1, 2}
+        );
+    }
+
+    public static List<int[]> input2() {
+        return asList(
+            new int[]{5, 4, 4},
+            new int[]{3, 4}
+        );
+    }
+
+    public static List<Integer> output() {
+        return asList(
+            29,
+            10
+        );
+    }
+}
