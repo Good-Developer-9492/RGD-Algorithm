ddec69c908b2c0c68f8172e8609d2ec4bf14dc60
hyunho058 <hyunho058@naver.com>
2023-02-20 19:49:42 +0900

    프로그래머스 카드뭉치 (https://school.programmers.co.kr/learn/courses/30/lessons/159994)

---
+++ src/main/java/Lv2/programmers/Q159994/Hyunho.java
@@ -0,0 +1,30 @@
+package Lv2.programmers.Q159994;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Hyunho {
+    //0.17ms, 83.2MB - 1.62ms, 78.6MB
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        Queue<String> card1Queue = new LinkedList<>(Arrays.asList(cards1));
+        Queue<String> card2Queue = new LinkedList<>(Arrays.asList(cards2));
+        for (String word : goal) {
+            String card1 = card1Queue.peek();
+            String card2 = card2Queue.peek();
+            if (verifySameWord(card1, word)) {
+                card1Queue.poll();
+                continue;
+            }
+            if (verifySameWord(card2, word)) {
+                card2Queue.poll();
+                continue;
+            }
+            return "No";
+        }
+        return "Yes";
+    }
+    private boolean verifySameWord(String s1, String s2){
+        return s1 != null && s1.equals(s2);
+    }
+}
---
+++ src/test/java/Lv2/programmers/Q159994/HyunhoTest.java
@@ -0,0 +1,22 @@
+package Lv2.programmers.Q159994;
+
+import Lv1.programmers.Q159994.Q159994;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution() {
+        var input1 = Q159994.input1();
+        var input2 = Q159994.input2();
+        var input3 = Q159994.input3();
+        var output = Q159994.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input1.get(i), input2.get(i), input3.get(i)));
+        }
+    }
+}
