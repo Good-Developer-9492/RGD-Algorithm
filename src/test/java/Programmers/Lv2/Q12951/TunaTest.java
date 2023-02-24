c8a5d166758079f11bc3743d2735584e64e9341c
Hagyun Kim <asdf0185@naver.com>
2023-02-24 09:24:19 +0900

    프로그래머스 12951 (#213)
    JadenCase 문자열 만들기 (https://school.programmers.co.kr/learn/courses/30/lessons/12951)

---
+++ src/test/java/Programmers/Lv2/Q12951/TunaTest.java
@@ -0,0 +1,20 @@
+package Programmers.Lv2.Q12951;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q12951.input();
+        var output = Q12951.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
