e5b27ce85f6624352dbf01a3c43fdd808059abe9
Hagyun Kim <asdf0185@naver.com>
2023-02-28 07:42:24 +0900

    프로그래머스 12924 (#225)
    숫자의 표현 (https://school.programmers.co.kr/learn/courses/30/lessons/12924)

---
+++ src/test/java/Programmers/Lv2/Q12924/TunaTest.java
@@ -0,0 +1,20 @@
+package Programmers.Lv2.Q12924;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q12924.input();
+        var output = Q12924.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
