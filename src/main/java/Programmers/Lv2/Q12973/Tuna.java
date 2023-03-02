34001bc626747967cae7abbbf2112fcf139a4d10
TunaHG <asdf0185@naver.com>
2023-03-03 08:53:39 +0900

    프로그래머스 짝지어 제거하기 (https://school.programmers.co.kr/learn/courses/30/lessons/12973)

---
+++ src/main/java/Programmers/Lv2/Q12973/Tuna.java
@@ -0,0 +1,28 @@
+package Programmers.Lv2.Q12973;
+
+import java.util.Stack;
+
+public class Tuna {
+    // (0.17ms ~ 68.21ms / 58.1MB ~ 85MB)
+    public int solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            if (stack.peek() == c) {
+                stack.pop();
+                continue;
+            }
+
+            stack.push(c);
+        }
+
+        if (stack.isEmpty()) return 1;
+
+        return 0;
+    }
+}
