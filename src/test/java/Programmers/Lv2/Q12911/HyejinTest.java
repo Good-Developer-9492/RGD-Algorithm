7454f94e25456e0429240d0119767fcd22b42da1
summerr0-0 <summerr060@gamil.com>
2023-03-02 13:16:42 +0900

    프로그래머스 12911

---
+++ src/test/java/Programmers/Lv2/Q12911/HyejinTest.java
@@ -0,0 +1,19 @@
+package Programmers.Lv2.Q12911;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyejinTest {
+    @Test
+    void solution() {
+        var input = Q12911.input();
+        var output = Q12911.output();
+
+        var algorithm = new Hyejin() ;
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+}
