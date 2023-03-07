eb6b71f077869d1bdeba3c27b415d5db115d2c47
Hagyun Kim <asdf0185@naver.com>
2023-03-08 07:53:36 +0900

    프로그래머스 42842 (#239)
    카펫 (https://school.programmers.co.kr/learn/courses/30/lessons/42842)

---
+++ src/test/java/Programmers/Lv2/Q42842/TunaTest.java
@@ -0,0 +1,21 @@
+package Programmers.Lv2.Q42842;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input1 = Q42842.input1();
+        var input2 = Q42842.input2();
+        var output = Q42842.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(algorithm.solution(input1.get(i), input2.get(i)), output.get(i));
+        }
+    }
+}
