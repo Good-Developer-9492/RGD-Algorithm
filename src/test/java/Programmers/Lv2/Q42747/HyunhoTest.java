727572caebc7c0734c2acf7edb42a4d6e157b87c
hyunho058 <hyunho058@naver.com>
2023-03-13 20:22:52 +0900

    프로그래머스 H-index (https://school.programmers.co.kr/learn/courses/30/lessons/42747)

---
+++ src/test/java/Programmers/Lv2/Q42747/HyunhoTest.java
@@ -0,0 +1,19 @@
+package Programmers.Lv2.Q42747;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @Test
+    void solution(){
+        var input = Q42747.input2();
+        var output = Q42747.output();
+
+        var algorithm = new Hyunho();
+
+        for (int i = 0; i < output.size(); i++) {
+            assertEquals(output.get(i), algorithm.solution(input.get(i)));
+        }
+    }
+}
