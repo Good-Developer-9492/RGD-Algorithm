bdf50dbfa85caface9f095b5146c04a57776aa0b
Hagyun Kim <asdf0185@naver.com>
2023-03-15 07:49:34 +0900

    프로그래머스 42747 (#256)
    H-Index (https://school.programmers.co.kr/learn/courses/30/lessons/42747)

--- src/test/java/Programmers/Lv2/Q42747/Q42747.java
+++ src/test/java/Programmers/Lv2/Q42747/Q42747.java
@@ -5,16 +5,17 @@ import java.util.List;
 import static java.util.Arrays.asList;
 
 public class Q42747 {
-    public static List<int[]> input2() {
+    public static List<int[]> input() {
         return asList(
-            new int[]{3,0,6,1,5}
-
+            new int[]{3, 0, 6, 1, 5},
+            new int[]{0, 1, 2, 3, 4}
         );
     }
 
     public static List<Integer> output() {
         return asList(
-            3
+            3,
+            2
         );
     }
 }
