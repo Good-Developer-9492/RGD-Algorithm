684414cb62905f50efb6ca023df0ac8d917d409b
TunaHG <asdf0185@naver.com>
2023-03-16 07:44:14 +0900

    프로그래머스 행렬의 곱셈 (https://school.programmers.co.kr/learn/courses/30/lessons/12949)

---
+++ src/test/java/Programmers/Lv2/Q12949/TunaTest.java
@@ -0,0 +1,21 @@
+package Programmers.Lv2.Q12949;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input1 = Q12949.input();
+        var input2 = Q12949.input2();
+        var output = Q12949.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(algorithm.solution(input1.get(i), input2.get(i)), output.get(i));
+        }
+    }
+}
