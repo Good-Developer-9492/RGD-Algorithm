042ba8069b3c8385b298717b1ee55beba82093c7
TunaHG <asdf0185@naver.com>
2023-03-20 08:21:44 +0900

    프로그래머스 위장 (https://school.programmers.co.kr/learn/courses/30/lessons/42578)

---
+++ src/test/java/Programmers/Lv2/Q42578/TunaTest.java
@@ -0,0 +1,20 @@
+package Programmers.Lv2.Q42578;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TunaTest {
+
+    @Test
+    void solution() {
+        var input = Q42578.input1();
+        var output = Q42578.output();
+
+        var algorithm = new Tuna();
+
+        for (int i = 0; i < input.size(); i++) {
+            assertEquals(algorithm.solution(input.get(i)), output.get(i));
+        }
+    }
+}
