2a6e5ed38731b3dcf8f0b705ea9631356ab461fc
summerr0-0 <120109736+summerr0-0@users.noreply.github.com>
2023-03-22 13:01:40 +0900

    프로그래머스 12949 행렬의 곱셈 (#267)

---
+++ src/test/java/Programmers/Lv2/Q12949/HyejinTest.java
@@ -0,0 +1,21 @@
+package Programmers.Lv2.Q12949;
+
+import Programmers.Lv2.Q12949.Q12949;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyejinTest {
+    @Test
+    void solution() {
+        var input1 = Q12949.input();
+        var input2 = Q12949.input2();
+        var output = Q12949.output();
+
+        var algorithm = new Hyejin();
+
+        for (int i = 0; i < input1.size(); i++) {
+            assertArrayEquals(output.get(i), algorithm.solution(input1.get(i), input2.get(i)));
+        }
+    }
+}
