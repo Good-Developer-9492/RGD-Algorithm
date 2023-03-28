58746fb092a5240a10ba76f2a369b5b1eb3c4a1b
TunaHG <asdf0185@naver.com>
2023-03-29 08:07:06 +0900

    프로그래머스 [1차] 뉴스 클러스터링 (https://school.programmers.co.kr/learn/courses/30/lessons/17677)

--- src/test/java/Programmers/Lv2/Q17677/TunaTest.java
+++ src/test/java/Programmers/Lv2/Q17677/TunaTest.java
@@ -1,15 +1,16 @@
 package Programmers.Lv2.Q17677;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TunaTest {
 
     @ParameterizedTest
     @MethodSource("Programmers.Lv2.Q17677.Q17677#inputAndResult")
-    void solution() {
+    void solution(String input1, String input2, int output) {
+        var algorithm = new Tuna();
+        assertEquals(output, algorithm.solution(input1, input2));
     }
 }
