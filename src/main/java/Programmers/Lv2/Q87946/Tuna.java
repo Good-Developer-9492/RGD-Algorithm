e4d567f12a0b0485372b4f464c3931f90485de2b
TunaHG <asdf0185@naver.com>
2023-04-03 15:23:53 +0900

    프로그래머스 피로도 (https://school.programmers.co.kr/learn/courses/30/lessons/87946)

--- src/main/java/Programmers/Lv2/Q87946/Tuna.java
+++ src/main/java/Programmers/Lv2/Q87946/Tuna.java
@@ -8,20 +8,20 @@ public class Tuna {
         check = new boolean[dungeons.length];
         for (int i = 0; i < dungeons.length; i++) {
             check[i] = true;
-            answer = Math.max(answer, getMaxAdventureCount(k - dungeons[i][1], dungeons, i));
+            answer = Math.max(answer, getMaxAdventureCount(k - dungeons[i][1], dungeons));
             check[i] = false;
         }
         return answer;
     }
 
-    private int getMaxAdventureCount(int k, int[][] dungeons, int location) {
+    private int getMaxAdventureCount(int k, int[][] dungeons) {
         int answer = 1;
 
         int recursiveCount = 0;
         for (int i = 0; i < dungeons.length; i++) {
             if (check[i] || dungeons[i][0] > k) continue;
             check[i] = true;
-            recursiveCount = Math.max(recursiveCount, getMaxAdventureCount(k - dungeons[i][1], dungeons, i));
+            recursiveCount = Math.max(recursiveCount, getMaxAdventureCount(k - dungeons[i][1], dungeons));
             check[i] = false;
         }
 
