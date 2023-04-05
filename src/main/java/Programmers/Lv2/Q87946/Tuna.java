7a77398cfacccbcbd9934442d3c8a1df4e985ea0
Hagyun Kim <asdf0185@naver.com>
2023-04-05 10:39:36 +0900

    프로그래머스 피로도 (https://school.programmers.co.kr/learn/courses/30/lessons/87946) (#302)

---
+++ src/main/java/Programmers/Lv2/Q87946/Tuna.java
@@ -0,0 +1,28 @@
+package Programmers.Lv2.Q87946;
+
+public class Tuna {
+    // (0.4ms / 74MB)
+    private static boolean[] check;
+    public int solution(int k, int[][] dungeons) {
+        int answer = Integer.MIN_VALUE;
+        check = new boolean[dungeons.length];
+        for (int i = 0; i < dungeons.length; i++) {
+            check[i] = true;
+            answer = Math.max(answer, getMaxAdventureCount(k - dungeons[i][1], dungeons));
+            check[i] = false;
+        }
+        return answer;
+    }
+
+    private int getMaxAdventureCount(int k, int[][] dungeons) {
+        int recursiveCount = 0;
+        for (int i = 0; i < dungeons.length; i++) {
+            if (check[i] || dungeons[i][0] > k) continue;
+            check[i] = true;
+            recursiveCount = Math.max(recursiveCount, getMaxAdventureCount(k - dungeons[i][1], dungeons));
+            check[i] = false;
+        }
+
+        return recursiveCount + 1;
+    }
+}
