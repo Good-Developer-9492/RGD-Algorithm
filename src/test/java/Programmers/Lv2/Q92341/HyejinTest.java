84954c34a85feabcf4bc86253fdec018f5345cd0
summerr0-0 <summerr060@gamil.com>
2023-04-07 16:04:27 +0900

    프로그래머스 92341

---
+++ src/test/java/Programmers/Lv2/Q92341/HyejinTest.java
@@ -0,0 +1,17 @@
+package Programmers.Lv2.Q92341;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HyejinTest {
+
+    @ParameterizedTest
+    @MethodSource("Programmers.Lv2.FirstWeekOfApril#Q92341")
+    void solution(int[] input1, String[] input2, int[] output) {
+        var algorithm = new Hyejin();
+        assertArrayEquals(output, algorithm.solution(input1, input2));
+    }
+
+}
