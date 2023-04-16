2d1f09892415d3479f502b42dff9981229db6d00
Hyunho Kim <hyunho058@naver.com>
2023-04-16 21:31:21 +0900

    프로그래머스 49993 - 완료 (#324)
    * 프로그래머스 스킬트리 (https://school.programmers.co.kr/learn/courses/30/lessons/49993)
    
    * 프로그래머스 스킬트리 (https://school.programmers.co.kr/learn/courses/30/lessons/49993)

---
+++ src/main/java/Programmers/Lv2/Q49993/Hyunho.java
@@ -0,0 +1,42 @@
+package Programmers.Lv2.Q49993;
+
+public class Hyunho {
+
+    //0.75ms, 78.9MB
+    public int solution(String skill, String[] skill_trees) {
+        int answer = 0;
+        String[] essentialSkill = skill.split("");
+
+        for (String skillTree : skill_trees) {
+            String[] skillArray = skillTree.split("");
+            int essentialSkillIdx = 0;
+
+
+            //코드수정 필요............
+            for (int j = 0; j < skillArray.length; j++) {
+                String s = skillArray[j];
+                if (j == skillArray.length - 1) {
+                    if (skill.contains(s)) {
+                        if (!essentialSkill[essentialSkillIdx].equals(s)) {
+                            break;
+                        }
+                    }
+                    answer++;
+                    continue;
+                }
+
+                if (!skill.contains(s)) {
+                    continue;
+                }
+
+                if (!essentialSkill[essentialSkillIdx].equals(s)) {
+                    break;
+                }
+
+                essentialSkillIdx++;
+            }
+        }
+
+        return answer;
+    }
+}
