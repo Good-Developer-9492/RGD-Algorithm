d336be166828f23184fe68bd27fd1e2186c50626
summerr0-0 <summerr060@gamil.com>
2023-04-18 20:01:55 +0900

    프로그래머스 84512 작업중

---
+++ src/main/java/Programmers/Lv2/Q84512/Hyejin.java
@@ -0,0 +1,12 @@
+package Programmers.Lv2.Q84512;
+
+public class Hyejin {
+    //A', 'E', 'I', 'O', 'U
+    //a -> aa -> aaa -> aaaa -> aaaaa  -> aaaae -> aaaai -> aaaao -> aaaau->  aaae -> aaai -> aaaao -> aaaau
+    String[] word = {"A","E","I","O","U"};
+    public int solution(String word) {
+        int answer = 0;
+        return answer;
+
+    }
+}
