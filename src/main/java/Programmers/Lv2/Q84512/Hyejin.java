f1a4e09e186289dd0b700c9306d0b12807d67fea
summerr0-0 <summerr060@gamil.com>
2023-04-21 18:49:09 +0900

    프로그래머스 84512

--- src/main/java/Programmers/Lv2/Q84512/Hyejin.java
+++ src/main/java/Programmers/Lv2/Q84512/Hyejin.java
@@ -1,12 +1,39 @@
 package Programmers.Lv2.Q84512;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hyejin {
+    //1.10ms, 73.7MB
+    //61.02ms, 80.9MB
     //A', 'E', 'I', 'O', 'U
     //a -> aa -> aaa -> aaaa -> aaaaa  -> aaaae -> aaaai -> aaaao -> aaaau->  aaae -> aaai -> aaaao -> aaaau
-    String[] word = {"A","E","I","O","U"};
+    String[] words = {"A", "E", "I", "O", "U"};
+    int length = 5;
+    List<String> dic = new ArrayList<>();
+
     public int solution(String word) {
-        int answer = 0;
-        return answer;
+        for (int i = 0; i < length; i++) {
+            dfs(words[i],word);
+        }
+        return dic.size();
+    }
+
+    public void dfs(String dicWord,String word) {
+        //기준 단어까지 다 만들어 졌을 때 빠져나가기
+        if(dic.contains(word)){
+            return;
+        }
+
+        if (!dic.contains(dicWord)) {
+            dic.add(dicWord);
+        }
+
+        if(dicWord.length()+1 > length) return;
+
+        for (int i = 0; i < length; i++) {
+            dfs(dicWord + words[i], word);
+        }
 
     }
 }
