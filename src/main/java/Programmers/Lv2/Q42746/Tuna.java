bbbfe37c3c568a4da4af33f45be0a1df57e57205
TunaHG <asdf0185@naver.com>
2023-04-28 09:10:04 +0900

    프로그래머스 가장 큰 수 (https://school.programmers.co.kr/learn/courses/30/lessons/42746)

--- src/main/java/Programmers/Lv2/Q42746/Tuna.java
+++ src/main/java/Programmers/Lv2/Q42746/Tuna.java
@@ -4,44 +4,19 @@ import java.util.Arrays;
 
 public class Tuna {
     public String solution(int[] numbers) {
+        // sort에 활용할 Integer 배열로 변환
         Integer[] temp = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        // sort
         Arrays.sort(temp, (n1, n2) -> {
             String s1 = String.valueOf(n1);
             String s2 = String.valueOf(n2);
-            System.out.println(s1 + " " + s2);
 
-            for (int i = 0; i < 4; i++) {
-                if (s1.charAt(i) > s2.charAt(i)) {
-                    return -1;
-                }
-
-                if (s1.charAt(i) < s2.charAt(i)) {
-                    return 1;
-                }
-
-                if (s1.charAt(i) == s2.charAt(i)) {
-                    if (s1.length() == i + 1 && s2.length() == i + 1) return 0;
-                    if (s1.length() == i + 1) {
-                        for (int j = i + 1; j < s2.length(); j++) {
-                            if (s1.charAt(i) < s2.charAt(j)) {
-                                return 1;
-                            }
-                        }
-                        return -1;
-                    }
-
-                    if (s2.length() == i + 1) {
-                        for (int j = i + 1; j < s1.length(); j++) {
-                            if (s1.charAt(j) > s2.charAt(i)) {
-                                return -1;
-                            }
-                        }
-                        return 1;
-                    }
-                }
-            }
-            return 0;
+            // 문자열 두개 합친 후 더 큰 문자열 사용
+            return (s2 + s1).compareTo(s1 + s2);
         });
+
+        // 첫 값이 0이면 나머지 전부 0
+        if (temp[0] == 0) return "0";
 
         StringBuilder sb = new StringBuilder();
         for (int number : temp) {
