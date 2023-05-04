3e25fa840f1b12e985ddc884e93ed0caabdd944d
TunaHG <asdf0185@naver.com>
2023-05-04 11:55:33 +0900

    프로그래머스 삼각 달팽이 (https://school.programmers.co.kr/learn/courses/30/lessons/68645)

---
+++ src/test/java/Programmers/Lv2/Q68645/TunaTest.java
@@ -0,0 +1,16 @@
+package Programmers.Lv2.Q68645;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class TunaTest {
+
+    @ParameterizedTest
+    @MethodSource("Programmers.Lv2.FirstWeekOfMay#Q68645")
+    void solution(int input, int[] output) {
+        var algorithm = new Tuna();
+        assertArrayEquals(output, algorithm.solution(input));
+    }
+}
