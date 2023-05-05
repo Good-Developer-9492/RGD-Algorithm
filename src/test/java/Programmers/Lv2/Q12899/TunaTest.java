48b24096f688390a851d14e3c180ff896a5534c0
TunaHG <asdf0185@naver.com>
2023-05-05 10:53:05 +0900

    프로그래머스 124 나라의 숫자 (https://school.programmers.co.kr/learn/courses/30/lessons/12899)

---
+++ src/test/java/Programmers/Lv2/Q12899/TunaTest.java
@@ -0,0 +1,16 @@
+package Programmers.Lv2.Q12899;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TunaTest {
+
+    @ParameterizedTest
+    @MethodSource("Programmers.Lv2.FirstWeekOfMay#Q12899")
+    void solution(int input, String output) {
+        var algorithm = new Tuna();
+        assertEquals(output, algorithm.solution(input));
+    }
+}
