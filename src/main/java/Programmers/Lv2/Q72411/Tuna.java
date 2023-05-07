72717781a49ee1f4df3966a277f760c2124c0b15
TunaHG <asdf0185@naver.com>
2023-05-08 08:26:41 +0900

    프로그래머스 메뉴 리뉴얼 (https://school.programmers.co.kr/learn/courses/30/lessons/72411)

--- src/main/java/Programmers/Lv2/Q72411/Tuna.java
+++ src/main/java/Programmers/Lv2/Q72411/Tuna.java
@@ -3,72 +3,57 @@ package Programmers.Lv2.Q72411;
 import java.util.*;
 
 public class Tuna {
-    // orders 별로 나올 수 있는 경우의 수 저장
-    private static Map<Integer, Set<String>> map;
-    // 전체 경우의 수
-    private static Set<String> set;
+    // (15ms / 80MB)
+    // course별로 나올수 있는 경우의 수와, 해당 경우의 수의 count
+    private static Map<Integer, Map<String, Integer>> map;
     // 전체 course
     private static Set<Integer> courses;
 
     public String[] solution(String[] orders, int[] course) {
         map = new HashMap<>();
-        set = new HashSet<>();
         courses = new HashSet<>();
 
         for (int num : course) {
+            map.put(num, new HashMap<>());
             courses.add(num);
         }
 
-        for (int i = 0; i < orders.length; i++) {
-            char[] tmp = orders[i].toCharArray();
+        for (String order : orders) {
+            char[] tmp = order.toCharArray();
             Arrays.sort(tmp);
-            recursive("", -1, tmp, i);
+            recursive("", -1, tmp);
         }
 
         List<String> list = new ArrayList<>();
-        for (String str : set) {
-            int cnt = 0;
-            for (int i = 0; i < orders.length; i++) {
-                if (cnt > 1) {
-                    list.add(str);
-                    break;
-                }
-
-                Set<String> orderSet = map.get(i);
-                if (orderSet.contains(str)) {
-                    cnt++;
-                }
+        for (int n : courses) {
+            int max = 0;
+            Map<String, Integer> tmp = map.get(n);
+            for (String key : tmp.keySet()) {
+                max = Math.max(max, tmp.get(key));
             }
-        }
 
-        List<String> list2 = new ArrayList<>(list);
-        for (String str : list) {
-            map.put(100, new HashSet<>());
-            recursive("", -1, str.toCharArray(), 100);
-            for (String tmp : map.get(100)) {
-                list.remove(tmp);
+            if (max < 2) continue;
+            for (String key : tmp.keySet()) {
+                if (tmp.get(key) == max) {
+                    list.add(key);
+                }
             }
         }
 
         String[] answer = list.toArray(String[]::new);
         Arrays.sort(answer);
-        for (String str : answer) {
-            System.out.println(str);
-        }
         return answer;
     }
 
     // 경우의 수 조합해주는 함수 (order가 주어지면, 해당 order에서 나올 수 있는 조합)
-    private static void recursive(String now, int nowIdx, char[] order, int idx) {
+    private static void recursive(String now, int nowIdx, char[] order) {
         if (courses.contains(now.length())) {
-            Set<String> idxSet = map.getOrDefault(idx, new HashSet<>());
-            idxSet.add(now);
-            map.put(idx, idxSet);
-            set.add(now);
+            int cnt = map.get(now.length()).getOrDefault(now, 0) + 1;
+            map.get(now.length()).put(now, cnt);
         }
 
         for (int i = nowIdx + 1; i < order.length; i++) {
-            recursive(now + order[i], i, order, idx);
+            recursive(now + order[i], i, order);
         }
     }
 }
