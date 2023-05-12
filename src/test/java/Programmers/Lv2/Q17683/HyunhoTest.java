295127341e23a6f3c2198f16d38c6a64098d4ffd
hyunho058 <hyunho058@naver.com>
2023-05-12 23:02:30 +0900

    프로그래머스 [3차] 방금그곡 (https://school.programmers.co.kr/learn/courses/30/lessons/17683)

---
+++ src/test/java/Programmers/Lv2/Q17683/HyunhoTest.java
@@ -0,0 +1,15 @@
+package Programmers.Lv2.Q17683;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HyunhoTest {
+    @ParameterizedTest
+    @MethodSource("Programmers.Lv2.SecondWeekOfMay#Q17683")
+    void solution(String input1, String[] input2, String output) {
+        var algorithm = new Hyunho();
+        assertEquals(output, algorithm.solution(input1, input2));
+    }
+}
