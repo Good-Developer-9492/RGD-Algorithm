55388699d0768e80206dfb73b1f839a6490d4ab2
summerr0-0 <summerr060@gmail.com>
2023-05-15 09:13:06 +0900

    프로그래머스 77485

---
+++ src/main/java/Programmers/Lv2/Q77485/Hyejin.java
@@ -0,0 +1,8 @@
+package Programmers.Lv2.Q77485;
+
+public class Hyejin {
+    public int[] solution(int rows, int columns, int[][] queries) {
+        int[] answer = {};
+        return answer;
+    }
+}
