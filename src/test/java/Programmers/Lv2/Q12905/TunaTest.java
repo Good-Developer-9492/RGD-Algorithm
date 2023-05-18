8994f221ee96ac946f94b7a1207f70ea1e9bca10
TunaHG <asdf0185@naver.com>
2023-05-19 08:19:49 +0900

    프로그래머스 가장 큰 정사각형 찾기 (https://school.programmers.co.kr/learn/courses/30/lessons/12905)

---
+++ src/test/java/Programmers/Lv2/Q12905/TunaTest.java
@@ -0,0 +1,16 @@
+package Programmers.Lv2.Q12905;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TunaTest {
+
+    @ParameterizedTest
+    @MethodSource("Programmers.Lv2.ThirdWeekOfMay#Q12905")
+    void solution(int[][] input, int output) {
+        var algorithm = new Tuna();
+        assertEquals(output, algorithm.solution(input));
+    }
+}
